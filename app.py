afe0ab7bfc2940982da045a5f143e24fb2a32ae7
AMNG95 <ange.mangamabada@gmail.com>
2025-02-09 22:09:39 +0000

    Ajout de mon application Streamlit

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import geopandas as gpd
+from shapely.geometry import Point
+from matplotlib.colors import ListedColormap
+import plotly.express as px
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, classification_report, precision_score, recall_score, roc_auc_score, auc
+from xgboost import XGBClassifier
+from sklearn.preprocessing import LabelEncoder
+from imblearn.over_sampling import SMOTE
+import xgboost
+import sklearn
+import gdown
+import os
+from google.colab import drive
